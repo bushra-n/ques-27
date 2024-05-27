f220491cf5a69a6d3112a9cc2dd47e07205a80da
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-05-27 09:13:35 +0500

    Add files via upload

---
+++ task27.js
@@ -0,0 +1,35 @@
+"use strict";
+// Task 27 Alien color#3  write three version of this program.
+// Alien color is green.
+let Alien_color = "green";
+if (Alien_color === "green") {
+    console.log("Player earned 5 points.");
+}
+else if (Alien_color === "yellow") {
+    console.log("Player earned 10 points.");
+}
+else if (Alien_color === "red") {
+    console.log("Player earned 15 points.");
+}
+//  Alien color is yellow.
+Alien_color = "yellow";
+if (Alien_color === "green") {
+    console.log("Player earned 5 points.");
+}
+else if (Alien_color === "yellow") {
+    console.log("Player earned 10 points.");
+}
+else if (Alien_color === "red") {
+    console.log("Player earned 15 points.");
+}
+// Alien color is red.
+Alien_color = "red";
+if (Alien_color === "green") {
+    console.log("Player earned 5 points.");
+}
+else if (Alien_color === "yellow") {
+    console.log("Player earned 10 points.");
+}
+else if (Alien_color === "red") {
+    console.log("Player earned 15 points.");
+}
